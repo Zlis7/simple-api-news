e4db353f21d048590e01a72b16c3d2bd38efe2c8
Zlis7 <denkalmy@gmail.com>
2025-03-27 22:54:45 +0500

    1.0.0

--- index.js
+++ index.js
@@ -8,42 +8,35 @@
         databaseURL: 'https://desktopnews-f7f75-default-rtdb.firebaseio.com'
     });
 
+    const db = firebase.database();
     const app = express();
     app.use(express.json());
 
     app.post('/login', async(req, res) => {
         try {
-            const userRecord = await defaultApp.auth().createUser ({
-                email: req.body['email'],
-                emailVerified: false,
-                phoneNumber: req.body['phone'],
-                password: req.body['password'],
-                displayName: req.body['name'],
-                photoURL: 'https://static.wikia.nocookie.net/disco-elysium3537/images/c/ce/%D0%A1%D0%B8%D0%BB%D0%B0_%D0%B2%D0%BE%D0%BB%D0%B8.png/revision/latest?cb=20210704100328&path-prefix=ru',
-                disabled: false
-            });
-            res.json({ content: true, uid: userRecord.uid });
+            const userRecord = await defaultApp.auth().getUserByEmail(req.body['email'])
+
+            res.json({ content: true, user: userRecord});
         } catch (error) {
-            console.error('Error creating new user:', error);
-            res.status(500).json({ error: 'Failed to create user' });
+            res.status(403).json({error:error.message});
         }
     });
-    app.post('/api/hello', (req, res) => {
-        res.json({ content: req.body});
+
+    app.post('/registration', async(req, res) => {
+        try {
+            const userRecord = await defaultApp.auth().createUser ({
+                email: req.body['email'],
+                password: req.body['password'],
+                emailVerified: false,
+                disabled: false
+            });
+
+            res.json({ content: true, uid: userRecord.uid });
+        } catch (error) {
+            res.status(403).json({error:error.message});
+        }
     });
-    app.post('/register', (req, res) => {
-        const {name, email, password} = req.body;
-        RegisterModel.findOne({email: email})
-        .then(user => {
-            if(user) {
-                res.json('Already have an account')
-            } else {
-                RegisterModel.create({name: name, email: email, password: password})
-                .then(result => res.json(result))
-                .catch(err => res.json(err))
-            }
-        }).catch(err => res.json(err))
-    });
+    
     
     app.listen(3001, () => {});
 
