76b5627915955fa7f653ab975a1a8aa1796f2c65
Zlis7 <denkalmy@gmail.com>
2026-01-14 22:07:24 +0500

    1.0.0

--- index.js
+++ index.js
@@ -1,82 +1,87 @@
-(()=>{
-    const express = require('express');
-    const firebase = require('firebase-admin');
-    const serviceAccount = require('./desktopnews-f7f75-firebase-adminsdk-fbsvc-7dbd6be92a.json');
+(() => {
+  const express = require("express");
+  const firebase = require("firebase-admin");
+  const serviceAccount = require("./desktopnews-f7f75-firebase-adminsdk-fbsvc-f951b49b05.json");
 
-    const defaultApp = firebase.initializeApp({
-        credential: firebase.credential.cert(serviceAccount),
-        databaseURL: 'https://desktopnews-f7f75-default-rtdb.firebaseio.com'
-    });
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://desktopnews-f7f75-default-rtdb.firebaseio.com",
+  });
 
-    const app = express();
-    app.use(express.json());
+  const app = express();
+  app.use(express.json());
 
-    let listAllUsers = [];
+  let listAllUsers = [];
 
-    const setlistAllUsers = async(nextPageToken) => {
-        await firebase.auth()
-          .listUsers(1000, nextPageToken)
-          .then((listUsersResult) => {
-            listUsersResult.users.forEach((userRecord) => {
-                listAllUsers.push(userRecord.toJSON());
-            });
-            if (listUsersResult.pageToken) {
-                setlistAllUsers(listUsersResult.pageToken);
-            }
-        })
-    };
-
-    app.get('/getNameAndPhotoUserByID', async(req, res) => {
-        if (req.query.apiKey != '7dbd6be92a7faeebcc1395f9f8c5d19dc77c8340'){
-            res.status(403).json({error: 'Invalid access key'});
-        };
-
-        const dataUser = await firebase.auth().getUser(req.query.uid)
-        res.status(200).json({ userName: dataUser.displayName, photoURL: dataUser.photoURL});
-    });
-
-    app.get('/listAllUsersByPageNumber', async(req, res) => {
-        if (req.query.apiKey != '7dbd6be92a7faeebcc1395f9f8c5d19dc77c8340'){
-            res.status(403).json({error: 'Invalid access key'});
-        };
-
-        listAllUsers = [];
-        await setlistAllUsers(req.query.page);
-
-        res.status(200).json({ listAllUsers: listAllUsers });
-    });
-
-    app.post('/disabelUserByID', async (req, res) => {
-        if (req.body['apiKey'] != '7dbd6be92a7faeebcc1395f9f8c5d19dc77c8340'
-            || req.body['apiKey'] === undefined || req.body['apiKey'] === null
-        ){
-            res.status(403).json({error: 'Invalid access key'});
-        };
-        
-        await firebase.auth()
-        .updateUser(req.body['uid'], {
-            disabled: req.body['disabled'],
-        })
-
-        res.status(200).json({ statusOperation: 'success' });
-    })
-
-    app.post('/updateUserByID', async (req, res) => {
-        if (req.body['apiKey'] != '7dbd6be92a7faeebcc1395f9f8c5d19dc77c8340'
-            || req.body['apiKey'] === undefined || req.body['apiKey'] === null
-        ){
-            res.status(403).json({error: 'Invalid access key'});
-        };
-
-        await firebase.database().ref('users').child(req.body['uid']).set({
-            age: req.body['age'],
-            role: req.body['role']
+  const setlistAllUsers = async (nextPageToken) => {
+    await firebase
+      .auth()
+      .listUsers(1000, nextPageToken)
+      .then((listUsersResult) => {
+        listUsersResult.users.forEach((userRecord) => {
+          listAllUsers.push(userRecord.toJSON());
         });
+        if (listUsersResult.pageToken) {
+          setlistAllUsers(listUsersResult.pageToken);
+        }
+      });
+  };
 
-        res.status(200).json({ statusOperation: 'success' });
-    })
+  app.get("/getNameAndPhotoUserByID", async (req, res) => {
+    if (req.query.apiKey != "7dbd6be92a7faeebcc1395f9f8c5d19dc77c8340") {
+      res.status(403).json({ error: "Invalid access key" });
+    }
 
-    
-    app.listen(3001, () => {});
-    module.exports = app;
+    const dataUser = await firebase.auth().getUser(req.query.uid);
+    res
+      .status(200)
+      .json({ userName: dataUser.displayName, photoURL: dataUser.photoURL });
+  });
+
+  app.get("/listAllUsersByPageNumber", async (req, res) => {
+    if (req.query.apiKey != "7dbd6be92a7faeebcc1395f9f8c5d19dc77c8340") {
+      res.status(403).json({ error: "Invalid access key" });
+    }
+
+    listAllUsers = [];
+    await setlistAllUsers(req.query.page);
+
+    res.status(200).json({ listAllUsers: listAllUsers });
+  });
+
+  app.post("/disabelUserByID", async (req, res) => {
+    if (
+      req.body["apiKey"] != "7dbd6be92a7faeebcc1395f9f8c5d19dc77c8340" ||
+      req.body["apiKey"] === undefined ||
+      req.body["apiKey"] === null
+    ) {
+      res.status(403).json({ error: "Invalid access key" });
+    }
+
+    await firebase.auth().updateUser(req.body["uid"], {
+      disabled: req.body["disabled"],
+    });
+
+    res.status(200).json({ statusOperation: "success" });
+  });
+
+  app.post("/updateUserByID", async (req, res) => {
+    if (
+      req.body["apiKey"] != "7dbd6be92a7faeebcc1395f9f8c5d19dc77c8340" ||
+      req.body["apiKey"] === undefined ||
+      req.body["apiKey"] === null
+    ) {
+      res.status(403).json({ error: "Invalid access key" });
+    }
+
+    await firebase.database().ref("users").child(req.body["uid"]).set({
+      age: req.body["age"],
+      role: req.body["role"],
+    });
+
+    res.status(200).json({ statusOperation: "success" });
+  });
+
+  app.listen(3001, () => {});
+  module.exports = app;
 })();
